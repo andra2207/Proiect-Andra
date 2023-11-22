476ff5c463b05fc1297a4f3f1e9cc083db07b7e8
Default <Default@DESKTOP-434VF04>
2023-11-22 23:09:59 +0200

    Add project files.

---
+++ Proiect Andra/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Proiect_Andra
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.SuspendLayout();
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(506, 2);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(298, 69);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "Quizz";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button4
+			// 
+			this.button4.BackgroundImage = global::Proiect_Andra.Properties.Resources.b9cff714d913a6a3fe05c9523f51e9ff;
+			this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button4.Location = new System.Drawing.Point(293, 181);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(211, 284);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "\r\n";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button3
+			// 
+			this.button3.BackgroundImage = global::Proiect_Andra.Properties.Resources.pcqoyq6ed5a91;
+			this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button3.Location = new System.Drawing.Point(506, 77);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(282, 373);
+			this.button3.TabIndex = 2;
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackgroundImage = global::Proiect_Andra.Properties.Resources.brian_bio41;
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button2.Location = new System.Drawing.Point(293, 2);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(211, 179);
+			this.button2.TabIndex = 1;
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.button1.Image = global::Proiect_Andra.Properties.Resources.Freddie_Mercury_Once_Revealed_Two_People_Hed_Like_To_Meet1;
+			this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.button1.Location = new System.Drawing.Point(2, 2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(285, 448);
+			this.button1.TabIndex = 0;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Formatia Queen";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+	}
+}
+
---
+++ Proiect Andra/Form6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proiect_Andra
+{
+	public partial class Form6 : Form
+	{
+		public Form6()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (radioButton2.Checked && radioButton6.Checked && radioButton7.Checked)
+				MessageBox.Show("FELICITARI!");
+			else
+				MessageBox.Show("MAI INCEARCA!");
+		}
+
+		
+	
+	}
+}
